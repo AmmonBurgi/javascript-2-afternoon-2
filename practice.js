8bf26a36485115215289885638c564ed38e49f93
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-02-13 17:06:40 -0700

    practice #2 complete

--- practice.js
+++ practice.js
@@ -18,6 +18,10 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(a){
+  return a[0]
+}
+first(arr);
 
 
 
@@ -33,6 +37,10 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(ab){
+  return ab[2]
+}
+last(arr);
 
 
 
@@ -48,6 +56,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for (let i = 0; i < family.length; i++){
+    alert(family[i]);
+  }
+}
+
 
 
 
@@ -63,6 +77,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for (let i = letters.length - 1; i >= 0; i--){
+  alert(letters[i])
+  }
+}
 
 
 
@@ -78,6 +97,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums){
+  let array = [];
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] % 2 === 0){
+      array.push(nums[i]);
+    }
+  }
+  return array
+}
 
 
 
